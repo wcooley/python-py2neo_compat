5195e39cfa578fd3ab7cd3486f7eec637bba6598
Wil Cooley <wcooley@nakedape.cc>
2017-10-14 10:08:20 -0700

    Update setup with my usual stuff

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 
 """The setup script."""
 
+from __future__ import absolute_import, print_function
+
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -12,32 +14,38 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'boltons',
+    'py2neo',
 ]
 
 setup_requirements = [
-    # TODO(wcooley): put setup requirements (distutils extensions, etc.) here
+    'setuptools_scm',
 ]
 
 test_requirements = [
+    'coverage',
     'pytest',
-    # TODO: put package test requirements here
+    'pytest-catchlog',
+    'pytest-cov',
+    'pytest-mock',
 ]
 
 setup(
     name='py2neo_compat',
     version='0.1.0',
+    use_scm_version=True,
     description="Compatibility layer for py2neo",
     long_description=readme + '\n\n' + history,
     author="Wil Cooley",
     author_email='wcooley@nakedape.cc',
     url='https://github.com/wcooley/py2neo_compat',
-    packages=find_packages(include=['py2neo_compat']),
+    package_dir={'': 'src',},
+    packages=find_packages(where='src', include=['py2neo_compat']),
     include_package_data=True,
     install_requires=requirements,
     license="Apache Software License 2.0",
     zip_safe=False,
-    keywords='py2neo_compat',
+    keywords='py2neo_compat py2neo compat',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -50,6 +58,9 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+    extras_require={
+        'test': test_requirements,
+    },
     test_suite='tests',
     tests_require=test_requirements,
     setup_requires=setup_requirements,
